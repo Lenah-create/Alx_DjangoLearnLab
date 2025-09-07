0e2265a61bfd79830841d6dbab2a5089794c434e
Lenah Karimi <lenahkarimi18@gmail.com>
2025-09-07 11:20:39 +0300

    Fix imports for automated check

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@
 from .models import Library
 from .models import Book
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Function-based view: list all books
 def list_books(request):
