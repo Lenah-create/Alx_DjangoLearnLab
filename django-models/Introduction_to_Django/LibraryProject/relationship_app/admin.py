3b5e59a11fe07e0eb638ef791c8daa8d43329a40
Lenah Karimi <lenahkarimi18@gmail.com>
2025-09-11 17:41:45 +0300

    Add views.py and templates for list_books

---
+++ django-models/Introduction_to_Django/LibraryProject/relationship_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Book, Library, Librarian, Author   # no UserProfile
+
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
+admin.site.register(Author)
+
---
+++ django-models/Introduction_to_Django/LibraryProject/relationship_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+# Class-based view for library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+
