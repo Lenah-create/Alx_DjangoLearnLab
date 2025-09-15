e9111a16da3f1b89bf08c90f5d93b63769ffb2ec
Lenah Karimi <lenahkarimi18@gmail.com>
2025-09-15 21:12:03 +0300

    Add models.py for bookshelf app

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from django.conf import settings
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
