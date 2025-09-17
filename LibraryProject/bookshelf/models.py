74460c92b131d112a2950fd3a1a3ff2e73f9df04
Lenah Karimi <lenahkarimi18@gmail.com>
2025-09-17 13:28:35 +0300

    Move apps and update structure for advanced features

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -32,3 +32,22 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+# ---------------------------
+# Book model with permissions
+# ---------------------------
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    # add other fields here if needed
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+            ("can_view", "Can view book"),
+        ]
+
+    def __str__(self):
+        return self.title
