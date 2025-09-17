d7f70a58b043b77636397647d460ba8c34c3c479
Lenah Karimi <lenahkarimi18@gmail.com>
2025-09-17 11:20:35 +0300

    Move bookshelf app from advanced_features_and_security to django-models and fix users/admin.py

--- advanced_features_and_security/users/admin.py
+++ advanced_features_and_security/users/admin.py
@@ -1,4 +1,3 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
@@ -8,7 +7,6 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ["username", "email", "date_of_birth", "is_staff", "is_active"]
     fieldsets = UserAdmin.fieldsets + (
-<<<<<<< HEAD:advanced_features_and_security/users/admin.py
         (None, {"fields": ("date_of_birth", "profile_photo")}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
@@ -16,12 +14,4 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
-=======
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-
->>>>>>> e9111a16da3f1b89bf08c90f5d93b63769ffb2ec:users/admin.py
 admin.site.register(CustomUser, CustomUserAdmin)
