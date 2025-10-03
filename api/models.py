55fb71cfde8377704b59fdd88a66cd7e99daf314
Lenah Karimi <lenahkarimi18@gmail.com>
2025-10-03 16:36:23 +0300

    Initial Django project with custom serializers

---
+++ api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Author(models.Model):
+    """
+    Author model to store writer information.
+    An Author can have multiple Books.
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Book model representing a written work.
+    Each Book is linked to one Author via a foreign key.
+    """
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(
+        Author, related_name="books", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
