536b29bcd635d0caca8ac199a400790393a17897
Lenah Karimi <lenahkarimi18@gmail.com>
2025-10-10 13:12:11 +0300

    Added comment CRUD URL patterns

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,12 +17,12 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
 
     # Blog Post CRUD URLs
-    path('post/', PostListView.as_view(), name='post-list'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    
      # Comment URLs
     path('posts/<int:post_id>/comments/new/', views.add_comment, name='add-comment'),
     path('comment/<int:comment_id>/update/', views.edit_comment, name='comment-update'),
